82e2158adb64329344861057d5e35fd68360b1c0
prasadsai <prasadsai0369@gmail.com>
2023-02-11 20:16:48 -0500

    Service Registry to inti

---
+++ service-registry/src/test/java/com/springmicroservices/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmicroservices.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
