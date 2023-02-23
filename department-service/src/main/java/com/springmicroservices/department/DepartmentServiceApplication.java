88e91f1eeb46ca31db6bd1f6f3ba9af2bca9df6e
prasadsai <prasadsai0369@gmail.com>
2023-02-22 19:31:33 -0500

    service registry added to manage all the microservices and directly communicate with each service instead of port

--- department-service/src/main/java/com/springmicroservices/department/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/springmicroservices/department/DepartmentServiceApplication.java
@@ -1,6 +1,7 @@
 package com.springmicroservices.department;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
